bdd16e773c6a13a565f4a835bbb044abc6663006
山口陽介 <yamaguchiyousuke@yamaguchiyousukes-MacBook-Pro.local>
2023-10-15 00:03:40 +0900

    チュートリアル確認

---
+++ HelloWorld.java
@@ -0,0 +1,30 @@
+// https://atcoder.jp/contests/practice/tasks/practice_1
+// A - Welcome to AtCoder
+import java.util.*;
+public class Main {
+	public static void main(String[] args){
+		Scanner sc = new Scanner(System.in);
+		// 整数の入力
+		int a = sc.nextInt();
+		// スペース区切りの整数の入力
+		int b = sc.nextInt();
+		int c = sc.nextInt();
+		// 文字列の入力
+		String s = sc.next();
+		// 出力
+		System.out.println((a+b+c) + " " + s);
+	}
+
+    public static void sampleMethod(String[] args){
+		Scanner sc = new Scanner(System.in);
+		// 整数の入力
+		int a = sc.nextInt();
+		// スペース区切りの整数の入力
+		int b = sc.nextInt();
+		int c = sc.nextInt();
+		// 文字列の入力
+		String s = sc.next();
+		// 出力
+		System.out.println((a+b+c) + " " + s);
+	}
+}
